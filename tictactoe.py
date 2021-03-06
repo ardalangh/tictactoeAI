b83f89176cf9e81803a9fb546fab2e4e39267f25
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-05 16:07:46 -0800

    utility fucntion done

--- tictactoe.py
+++ tictactoe.py
@@ -94,7 +94,12 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if terminal(board):
+        win = winner(board)
+        if  win == X: return 1
+        elif win == O: return -1
+        else: return 0
+        
 
 
 def minimax(board):
